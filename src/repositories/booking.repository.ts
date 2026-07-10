8178c0ef0271b5b340d61c8068da87a0ddb72d59
Xtract01 <meena.balwir149@gmail.com>
2026-07-11 01:50:24 +0530

    feat(prisma): add finalized field to IdempotencyKey model and create utility for generating idempotency keys

--- src/repositories/booking.repository.ts
+++ src/repositories/booking.repository.ts
@@ -1,9 +1,18 @@
+import type { Prisma, Booking } from "../generated/prisma/client";
 import { prisma } from "../prisma/client";
-import type { Prisma } from "../generated/prisma/client";
 
 export async function createBooking(bookingInput: Prisma.BookingCreateInput) {
   const booking = await prisma.booking.create({
     data: bookingInput,
   });
   return booking;
+}
+
+export async function createIdempotencyKey(key: string, booking?: Booking) {
+  const idempotencyKey = await prisma.idempotencyKey.create({
+    data: {
+      key,
+    },
+  });
+  return idempotencyKey;
 }
