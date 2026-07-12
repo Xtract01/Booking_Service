4128f2fcd377e53632d1be05c97a1308eb7478ce
Xtract01 <meena.balwir149@gmail.com>
2026-07-13 02:46:32 +0530

    feat(prisma): establish relationship between Booking and IdempotencyKey models by adding bookingId and updating related functions

--- src/repositories/booking.repository.ts
+++ src/repositories/booking.repository.ts
@@ -8,11 +8,49 @@ export async function createBooking(bookingInput: Prisma.BookingCreateInput) {
   return booking;
 }
 
-export async function createIdempotencyKey(key: string, booking?: Booking) {
+export async function createIdempotencyKey(key: string, bookingId: number) {
   const idempotencyKey = await prisma.idempotencyKey.create({
     data: {
+      key,
+      booking: {
+        connect: {
+          id: bookingId,
+        },
+      },
+    },
+  });
+  return idempotencyKey;
+}
+
+export async function getIdempotencyKey(key: string) {
+  const idempotencyKey = await prisma.idempotencyKey.findUnique({
+    where: {
       key,
     },
   });
   return idempotencyKey;
+}
+
+export async function gerBookingById(bookingId: number) {
+  const booking = await prisma.booking.findUnique({
+    where: {
+      id: bookingId,
+    },
+  });
+  return booking;
+}
+
+export async function confirmBooking(
+  bookingId: number,
+  status: Prisma.EnumBookingStatusFieldUpdateOperationsInput,
+) {
+  const booking = await prisma.booking.update({
+    where: {
+      id: bookingId,
+    },
+    data: {
+      status: status,
+    },
+  });
+  return booking;
 }
